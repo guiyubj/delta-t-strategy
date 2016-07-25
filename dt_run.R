367ae37d7508db90bd082867438a69746cae6235
guiyubj <yuguicode@gmail.com>
2016-07-25 14:36:04 +0800

    change location of result.csv

--- dt_run.R
+++ dt_run.R
@@ -5,21 +5,21 @@
 
 #define parameters
 str_idx = 'pri' #'pri' for Wind primary industry index, 'scd' for Wind secondary industry index
-max_dt = 0 #maximum time delay between two timeseries
+max_dt = 100 #maximum time delay between two timeseries
 
 
 #run the whole process
 
 #get data
 print('Getting data...')
-w_wsd_data = get_data('pri')
+w_wsd_data = get_data(str_idx)
 print('Done')
 
 #calc corr_dt_matrix
 print('Calculating corr_dt_matrix...')
-corr_dt_matrix = find_corr_delta_t_matrix(w_wsd_data, max_dt, 'pri')
+corr_dt_matrix = find_corr_delta_t_matrix(w_wsd_data, max_dt, str_idx)
 print('Done')
 #write the matrix to csv
-str_csv_name = paste('corr_dt_matrix_', str_idx, max_dt, '.csv', sep = '')
+str_csv_name = paste('./result/corr_dt_matrix_', str_idx, max_dt, '.csv', sep = '')
 write.csv(corr_dt_matrix, str_csv_name)
 print(paste('Written to CSV:', str_csv_name))
